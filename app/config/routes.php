ba19e48fccee56241112cc81af298fd7a3a6ba7a
Feno2105 <mamyrandria2105@gmail.com>
2025-01-17 19:29:39 +0300

    c

--- app/config/routes.php
+++ app/config/routes.php
@@ -18,19 +18,11 @@ use flight\net\Router;
 
 // $Welcome_Controller = new WelcomeController();
 // $router->get('/home', [ $Welcome_Controller, 'home' ]); 
-$Product = new Product();
-$router->get('/', [ $Product, 'list' ]); 
+
 //$router->get('/', [ 'WelcomeController', 'home' ]); 
 
 //$router->get('/', \app\controllers\WelcomeController::class.'->home'); 
 
 $router->get('/hello-world/@name', function($name) {
 	echo '<h1>Hello world! Oh hey '.$name.'!</h1>';
-});
-
-$router->group('/api', function() use ($router,$app) {
-	$Api_Example_Controller = new ApiExampleController($app);
-	$router->get('/users', [ $Api_Example_Controller, 'getUsers' ]);
-	$router->get('/users/@id:[0-9]', [ $Api_Example_Controller, 'getUser' ]);
-	$router->post('/users/@id:[0-9]', [ $Api_Example_Controller, 'updateUser' ]);
 });
---
+++ app/models/CrudModel.php
@@ -0,0 +1,49 @@
+<?php 
+namespace app\models;
+use Flight;
+use PDO;
+
+class CrudModel 
+{
+    private $db;
+    public function __construct($var) {
+        $this->db = $var;
+    }
+    public function AjoutHabitation($idType,$nbChambres,$loyer,$photo,$quartier,$desc){
+        $sql = "INSERT INTO habitation(idType,nbChambre,loyer,photo,quartier,desc) VALUES(:idType,:nbChambres,:loyer,:photo,:quartier,:desc)";
+        $stt = $this->db->prepare($sql);
+        $stt->execute([
+            ':idType' => $idType,
+            ':nbChambres' => $nbChambres,
+            ':loyer' => $loyer,
+            ':photo' => $photo,
+            ':quartier' => $quartier,
+            ':desc' => $desc
+        ]);
+    }
+    public function ListHabitation(){
+        $sql = "SELECT * FROM habitation";
+        $stt = $this->db->prepare($sql);
+        $stt->execute();
+    }
+    public function ModifierHabitation($idHabitation,$idType,$nbChambres,$loyer,$photo,$quartier,$desc){
+        $sql = "UPDATE habitation SET idType = :idType AND nbChambre = :nbChambres AND loyer = :loyer AND photo = :photo AND quartier = :quartier AND desc = :desc WHERE idHabitation = :idHabitation";
+        $stt = $this->db->prepare($sql);
+        $stt->execute([
+            ':idHabitation' => $idHabitation,
+            ':idType' => $idType,
+            ':nbChambres' => $nbChambres,
+            ':loyer' => $loyer,
+            ':photo' => $photo,
+            ':quartier' => $quartier,
+            ':desc' => $desc
+        ]);
+    }
+    public function DeleteHabitation($idHabitation){
+        $sql = "DELETE FROM habitation WHERE idHabitation = :idHabitation";
+        $stt = $this->db->prepare($sql);
+        $stt->execute([
+            'idHabitation' => $idHabitation
+        ]);
+    }
+}
